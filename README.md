e2a8a71ca8aa6c0514fb9558a6bac58c16da195d
Daniel Rodriguez <daniel@danielfrg.com>
2020-12-01 18:14:32 -0500

    Update env management and add configurable endpoint

--- README.md
+++ README.md
@@ -9,21 +9,52 @@ Deploy MLflow models to Algorithmia
 ## Install
 
 ```
-pip install git+git://github.com/algorithmia/mlflor-algorithmia.git
+pip install git+git://github.com/algorithmia/mlflow-algorithmia.git
+
+pip install mlflow-algorithmia
 ```
 
-## Example usage from sklearn
+## Usage
+
+This is based on the [mlflow tutorial](https://www.mlflow.org/docs/latest/tutorials-and-examples/tutorial.html)
+we reproduce some steps here but for more details look at the official [mlflow docs](https://www.mlflow.org/docs).
 
 ```
-# Create model
 python examples/sklearn_elasticnet_wine/train.py
+```
 
-# This will create a mlruns dir
-# <path-to-models>: mlruns/0/b3c1d94f189945779174c0d8304660d3/artifacts/model
+This will create an `mlruns` directory that contains the trained model, you can
+view the UI running `mlflow ui` and start the mlflow server running:
+
+```
+$ mlflow models serve -m mlruns/0/<run-id>/artifacts/model -p 1234
+
+# Make a test query
+
+$ curl -X POST -H "Content-Type:application/json; format=pandas-split" --data '{"columns":["alcohol", "chlorides", "citric acid", "density", "fixed acidity", "free sulfur dioxide", "pH", "residual sugar", "sulphates", "total sulfur dioxide", "volatile acidity"],"data":[[12.8, 0.029, 0.48, 0.98, 6.2, 29, 3.33, 1.2, 0.39, 75, 0.66]]}' http://127.0.0.1:1234/invocations
+
+[5.120775719594933]
+```
+
+Now let's deploy the same endpoint in Algorithmia. You will need:
+1. An Algorithmia API key with `Read + Write` data access
+2. The path to the model you want to deploy (under `mlruns`)
+
+```
+# Set your Algorithmia API key
+export ALGORITHMIA_USERNAME=<username>
+export ALGORITHMIA_API_KEY=<generated-api-key>
 
 # Create a deployment
-mlflow deployments create --name <name> -t algorithmia -m <path-to-model>
+mlflow deployments create -t algorithmia --name mlflow_sklearn_demo -m <path-to-model>
 
-# Update deployment (after running the model again)
-mlflow deployments update --name <name> -t algorithmia -m <path-to-model>
+# Test query
+
+curl -X POST -d '{"columns":["alcohol", "chlorides", "citric acid", "density", "fixed acidity", "free sulfur dioxide", "pH", "residual sugar", "sulphates", "total sulfur dioxide", "volatile acidity"],"data":[[12.8, 0.029, 0.48, 0.98, 6.2, 29, 3.33, 1.2, 0.39, 75, 0.66]]}' -H 'Content-Type: text/plain' -H 'Authorization: Simple simkgOvmGdKhVfU7JvbYBCq7Yxu1' https://api.test.algorithmia.com/v1/algo/danielfrg/mlflow_sklearn_demo5?timeout=300
+```
+
+To update deployment for example after training a new model
+
+```
+mlflow deployments update -t algorithmia --name mlflow_sklearn_demo -m <path-to-new-model>
 ```
--- mlflow_algorithmia/deployment.py
+++ mlflow_algorithmia/deployment.py
@@ -4,13 +4,13 @@ import yaml
 import urllib
 import logging
 import tarfile
+from urllib.parse import urlparse
 
 import Algorithmia
 from git import Git, Repo, remote
 from jinja2 import Environment, FileSystemLoader
 from mlflow.exceptions import MlflowException
 from mlflow.deployments import BaseDeploymentClient
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -24,7 +24,7 @@ logger.addHandler(handler)
 
 DEPLOYMENT_NAME = "algorithmia"
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
-TMP_DIR = "./algo_tmp/"
+TMP_DIR = "./algorithmia_tmp/"
 
 
 class AlgorithmiaDeploymentClient(BaseDeploymentClient):
@@ -35,14 +35,16 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
         self.settings = Settings()
 
         if self.settings["api_key"] is None:
-            e = "Environment variable: ALGO_API_KEY is not set."
-            raise MlflowException(e)
+            raise MlflowException(
+                "Environment variable: ALGORITHMIA_API_KEY is not set."
+            )
 
         if self.settings["username"] is None:
-            e = "Environment variable: ALGO_USERNAME is not set."
-            raise MlflowException(e)
+            raise MlflowException(
+                "Environment variable: ALGORITHMIA_USERNAME is not set."
+            )
 
-        self.algo_client = Algorithmia.client(self.settings["api_key"])
+        self.client = Algorithmia.client(self.settings["api_key"])
 
     def create_deployment(self, name, model_uri, flavor=None, config=None):
         """
@@ -64,13 +66,16 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
             "tagline": self.settings["tagline"],
         }
         settings = {
-            "source_visibility": "closed",
+            # "environment": "cpu",
+            # "language": "python3-1",
             "package_set": "python37",
-            "license": "apache2",
+            "source_visibility": "closed",
+            "license": "apl",
             "network_access": "full",
             "pipeline_enabled": True,
         }
-        self.algo_client.algo(algo_namespace).create(details, settings)
+
+        self.client.algo(algo_namespace).create(details=details, settings=settings)
 
         self.update_deployment(
             name=name, model_uri=model_uri, flavor=flavor, config=config
@@ -84,11 +89,12 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
         """
         self.read_mlmodel(model_uri)
 
+        os.makedirs(TMP_DIR, exist_ok=True)
         tar_file = self.compress_mlflow(model_uri)
-        algo_tar_file = self.algo_upload_model(name, tar_file)
+        algo_tar_file = self.upload_model(name, tar_file)
 
         repo_path = self.clone_algorithm_repo(name)
-        self.update_code(name, repo_path, mlflow_bundle=algo_tar_file)
+        self.update_source(name, repo_path, mlflow_bundle=algo_tar_file)
         self.update_repo()
         return {"flavor": flavor}
 
@@ -116,14 +122,13 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
 
         if not os.path.exists(repo_path):
             # Encoding the API key, so we can use it in the git URL
-            encoded_api_key = urllib.parse.quote_plus(self.settings["api_key"])
 
-            algo_repo = "https://{}:{}@git.algorithmia.com/git/{}/{}.git".format(
-                self.settings["username"],
-                encoded_api_key,
-                self.settings["username"],
-                name,
-            )
+            username = self.settings["username"]
+            encoded_api_key = urllib.parse.quote_plus(self.settings["api_key"])
+            git_endpoint = self.settings["git_endpoint"]
+            username = self.settings["username"]
+            algo_name = name
+            algo_repo = f"https://{username}:{encoded_api_key}@{git_endpoint}/git/{username}/{algo_name}.git"
             self.repo = Repo.clone_from(algo_repo, repo_path, progress=Progress())
         else:
             self.repo = Repo(repo_path)
@@ -134,7 +139,7 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
 
     def update_repo(self):
         print("Updating Algorithmia repo and building model")
-        commit_msg = f"Update MLflow run_id: {self.run_id}"
+        commit_msg = f"Update - MLflow run_id: {self.run_id}"
         self.repo.git.add(".")
         self.repo.index.commit(commit_msg)
         origin = self.repo.remote(name="origin")
@@ -164,31 +169,28 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
 
         return tar_fpath
 
-    def algo_upload_model(self, name, tar_fpath):
+    def upload_model(self, name, tar_fpath):
         """
         Upload compress MLflow model to algorithmia
         """
         username = self.settings["username"]
         algo_data_dir = f"data://{username}/{name}"
 
-        if not self.algo_client.dir(algo_data_dir).exists():
-            self.algo_client.dir(algo_data_dir).create()
+        if not self.client.dir(algo_data_dir).exists():
+            self.client.dir(algo_data_dir).create()
 
         tar_fname = os.path.basename(tar_fpath)
         algo_file = os.path.join(algo_data_dir, tar_fname)
-        self.algo_client.file(algo_file).putFile(tar_fpath)
+        self.client.file(algo_file).putFile(tar_fpath)
         print(f"Uploaded MLflow bundle to: {algo_file}")
         return algo_file
 
-    def update_code(self, name, repo_path, **kwargs):
+    def update_source(self, name, repo_path, **kwargs):
         _ = os.path.join(repo_path, ".gitignore")
         self.render_file("gitignore_repo", _, **kwargs)
 
-        _ = os.path.join(repo_path, "models")
-        os.makedirs(_, exist_ok=True)
-
-        _ = os.path.join(repo_path, "models", ".gitignore")
-        self.render_file("gitignore_all", _, **kwargs)
+        _ = os.path.join(repo_path, "requirements.txt")
+        self.render_file("requirements.txt", _, **kwargs)
 
         _ = os.path.join(repo_path, "src", f"{name}.py")
         self.render_file("main.py", _, **kwargs)
@@ -198,6 +200,12 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
 
         _ = os.path.join(repo_path, "src", "mlflow_wrapper.py")
         self.render_file("mlflow_wrapper.py", _, **kwargs)
+
+        _ = os.path.join(repo_path, "models")
+        os.makedirs(_, exist_ok=True)
+
+        _ = os.path.join(repo_path, "models", ".gitignore")
+        self.render_file("gitignore_all", _, **kwargs)
 
     def render_file(self, inp, out, **kwargs):
         file_loader = FileSystemLoader(os.path.join(THIS_DIR, "templates"))
@@ -212,10 +220,18 @@ class AlgorithmiaDeploymentClient(BaseDeploymentClient):
 class Settings(dict):
     def __init__(self):
         super().__init__()
-        self["api_key"] = os.environ.get("ALGO_API_KEY", None)
-        self["username"] = os.environ.get("ALGO_USERNAME", None)
-        self["tagline"] = os.environ.get("ALGO_TAGLINE", "")
-        self["summary"] = os.environ.get("ALGO_SUMMARY", "")
+        self["api_endpoint"] = os.environ.get(
+            "ALGORITHMIA_API", "https://api.algorithmia.com"
+        )
+        self["api_key"] = os.environ.get("ALGORITHMIA_API_KEY", None)
+        self["username"] = os.environ.get("ALGORITHMIA_USERNAME", None)
+        self["tagline"] = os.environ.get("ALGORITHM_TAGLINE", "Mlflow deployment")
+        self["summary"] = os.environ.get("ALGORITHN_SUMMARY", "MLflow deployment")
+
+        url = urlparse(self["api_endpoint"]).netloc
+        git_endpoint = url[4:] if url.startswith("www.") else url
+        git_endpoint = git_endpoint.replace("api.", "git.")
+        self["git_endpoint"] = git_endpoint
 
 
 class Progress(remote.RemoteProgress):
